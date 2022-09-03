f5bdae18c3d38423418fef13b981aa2254be6114
vlandero <vlad.andrei.rosu@gmail.com>
2022-09-03 19:13:41 +0300

    removed comms

--- main.c
+++ main.c
@@ -3,28 +3,9 @@
 #include <stdbool.h>
 #include <time.h>
 
-/*
-
-A 7 K 7 7
-7 A A A T
-A 7 K Q Q
-
-- 7 K 7 7
-7 - - - T
-A 7 K Q Q
-
-A 7 K 7 7
-7 - - - T
-- 7 K Q Q
-
-*/
-
-//dreapta sus i = i - 1, j = j + 1
-//dreapta     i = i    , j = j + 1
-//dreapta jos i = i + 1, j = j + 1
 
 const char simboluri[]    = {'A','7','K','Q','C','0','3'};
-const float multipliers[] = { 2 , 10, 3 , 4 , 6 ,1.5, 5 }; ///mai gandeste te la mize, e cam broken jocu
+const float multipliers[] = { 2 , 10, 3 , 4 , 6 ,1.5, 5 };
 const int lenSimboluri = 7;
 
 char** pacanea;
@@ -40,18 +21,10 @@ void afisarePacanea(){
     printf("\n");
 }
 
-/*
-
-0,0 -> 1,1 -> 1,2 -> 1,3
-
-- - A 7 7
-7 K K K T
-A 7 K Q Q
-*/
 
 void cautareCombinatie(int i, int j, int nr, int* total){
     nr++;
-    if(j + 1 == coloane){ //conditie de oprire (sunt pe ultimul caracter al unei combinatii)
+    if(j + 1 == coloane){
         if(nr < 3)
             nr = 0;
         else{
@@ -64,28 +37,28 @@ void cautareCombinatie(int i, int j, int nr, int* total){
         return;
     }
     bool terminatCombinatie = true;
-    if(i - 1 >= 0 && pacanea[i][j] == pacanea[i-1][j+1]){ //sus
+    if(i - 1 >= 0 && pacanea[i][j] == pacanea[i-1][j+1]){
         char c = pacanea[i][j];
         pacanea[i][j] = '-';
         cautareCombinatie(i - 1, j + 1, nr, total);
         pacanea[i][j] = c;
         terminatCombinatie = false;
     }
-    if(pacanea[i][j] == pacanea[i][j+1]){ //dreapta
+    if(pacanea[i][j] == pacanea[i][j+1]){
         char c = pacanea[i][j];
         pacanea[i][j] = '-';
         cautareCombinatie(i, j + 1, nr, total);
         pacanea[i][j] = c;
         terminatCombinatie = false;
     }
-    if(i + 1 < linii && pacanea[i][j] == pacanea[i+1][j+1]){ //jos
+    if(i + 1 < linii && pacanea[i][j] == pacanea[i+1][j+1]){
         char c = pacanea[i][j];
         pacanea[i][j] = '-';
         cautareCombinatie(i + 1, j + 1, nr, total);
         pacanea[i][j] = c;
         terminatCombinatie = false;
     }
-    if(terminatCombinatie == true) { //conditie de oprire (sunt pe ultimul caracter al unei combinatii)
+    if(terminatCombinatie == true) {
         if(nr < 3)
             nr = 0;
         else{
@@ -121,7 +94,7 @@ void joc(float* balanta, float miza){
         if(simboluri[i] == pacanea[0][0])
             break;
     }
-    castig = castig + total * multipliers[i] * miza; ///am uitat sa scriem * miza, aici am facut doar multiplier, nu l-am inmultit cu miza
+    castig = castig + total * multipliers[i] * miza;
 
     total = 0;
     cautareCombinatie(1,0,0,&total);
@@ -129,7 +102,7 @@ void joc(float* balanta, float miza){
         if(simboluri[i] == pacanea[1][0])
             break;
     }
-    castig = castig + total * multipliers[i] * miza; ///am uitat sa scriem * miza, aici am facut doar multiplier, nu l-am inmultit cu miza
+    castig = castig + total * multipliers[i] * miza;
 
     total = 0;
     cautareCombinatie(2,0,0,&total);
@@ -137,21 +110,20 @@ void joc(float* balanta, float miza){
         if(simboluri[i] == pacanea[2][0])
             break;
     }
-    castig = castig + total * multipliers[i] * miza; ///am uitat sa scriem * miza, aici am facut doar multiplier, nu l-am inmultit cu miza
-    *balanta += castig; ///am uitat sa adaug castigul la balanta
+    castig = castig + total * multipliers[i] * miza;
+    *balanta += castig;
 
-    printf("Ai castigat %f! Joaca responsabil\n",castig); ///afisam castigul pentru fiecare joc
+    printf("Ai castigat %f! Joaca responsabil\n",castig);
 
 }
 
 int main()
 {
-    ///am adaugat un text cu toti multiplierii
+
     printf("Multipliers:\n");
     for(int i = 0; i < lenSimboluri; i++){
         printf("%c -> %f x\n",simboluri[i],multipliers[i]);
     }
-    ///pana aici
 
     srand(time(NULL));
 
@@ -165,7 +137,7 @@ int main()
     if(linii < 3){ ///trebuie introduse cel putin 3 linii
         printf("Data viitoare asculta indicatiile.\n"); ///
         printf("Apasa orice pentru a continua\n"); ///
-        scanf("%c",&c); /// am adaugat astea doar pentru executabil, nu trebuie neaparat incluse in proiect
+        scanf("%c",&c);
         return 0;
     }
     pacanea = (char**)malloc(linii * sizeof(char*));
@@ -174,21 +146,21 @@ int main()
 
     printf("Introdu balanta\n");
     scanf("%f",&balanta);
-    scanf("%c",&c); //citesc enter sa nu ramana necitit
+    scanf("%c",&c);
 
     bool running = true;
 
     printf("Seteaza miza si incepe sa joci\n");
     scanf("%f",&miza);
-    scanf("%c",&c); //citesc enter sa nu ramana necitit
+    scanf("%c",&c);
 
     while(running){
 
-        printf("Balanta: %f\nMiza curenta: %f\n",balanta,miza); ///facem balanta si miza sa se vada la fiecare rotire
+        printf("Balanta: %f\nMiza curenta: %f\n",balanta,miza);
 
         joc(&balanta,miza);
 
-        printf("Noua balanta: %f\n",balanta); ///afisam si noua balanta, dupa joc
+        printf("Noua balanta: %f\n",balanta);
 
         printf("Apasa q pentru a iesi\n");
         printf("Apasa s pentru a schimba miza\n");
@@ -198,27 +170,25 @@ int main()
 
         if(c == 'q'){
             running = false;
-            scanf("%c",&c); //citesc enter sa nu ramana necitit
-            printf("Ma bucur ca te-ai distrat. Te mai asteptam. Joaca responsabil!\n"); ///add text misto
+            scanf("%c",&c);
+            printf("Ma bucur ca te-ai distrat. Te mai asteptam. Joaca responsabil!\n");
         }
         else if(c == 's'){
-            scanf("%c",&c); //citesc enter sa nu ramana necitit
-            printf("Introdu miza\n"); ///introducem miza
-            scanf("%f",&miza); ///
-            scanf("%c",&c); ///citesc enter sa nu ramana necitit
+            scanf("%c",&c);
+            printf("Introdu miza\n");
+            scanf("%f",&miza);
+            scanf("%c",&c);
         }
         else if(c == '\n')
             printf("Continuam. Joaca responsabil!\n");
 
-        ///mesaj 0 lei
         if(balanta == 0){
             running = false;
             printf("Nu ai jucat responsabil, dar noi tot ne-am facut banii. Va mai asteptam. GGEZ\n");
         }
-        ///pana aici
 
     }
-    printf("Apasa orice pentru a continua\n"); ///
-    scanf("%c",&c); /// am adaugat astea doar pentru executabil, nu trebuie neaparat incluse in proiect
+    printf("Apasa orice pentru a continua\n");
+    scanf("%c",&c);
     return 0;
 }
